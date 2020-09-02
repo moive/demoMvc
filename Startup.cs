5abd48508a1f5a8534b19c2a5b493223de51ece0
Moises Velasquez <mvelasquez@pictoric.com>
2020-09-01 22:48:38 -0500

    project init demoMvc

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(demoMvc.Startup))]
+namespace demoMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
