376a51eccc2e063d0ae5139c83fa87516201822b
Moises Velasquez <mvelasquez@pictoric.com>
2020-09-01 22:50:04 -0500

    RouteConfig

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace demoMvc
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "RuteoEjemplo",
+                url: "Ejemplo",
+                defaults: new { controller = "Home", action = "Contact"}
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
